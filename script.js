1c2630f69460239dfe1c30cc503221eb5a940dd8
Negin <nuggetnegin@gmail.com>
2019-11-12 20:32:19 -0500

    update:
    randomized cat area, check if user clicked correct area -
    keep count
    tracking happiness

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-
-
 /*cat game object, has happy level which starts at 0 and will be incremented later
 has an array of the cat body which will be selected during randomizer method*/
-catGame = { 
+const catGame = { 
     happiness: 0, 
     catBody: ['head','back','tail','frontLegs','belly','backLegs'],
-    correctPetCount: 0 /*global scope so it doesnt reset*/
+    correctPetCount: 0,
+    favoriteBodyPart: null
 }
 
-const userEvents = () => {
+const getRandomBodyPart = (catBody) => {
+    /*goes from 0 to 5 to correspond with array elements of catBody*/
+    let random = Math.floor((Math.random() * 6)); 
+    return catBody[random]; /*returning the randomized body part*/
+}
+
+/*setting favorite body part from cat obj to the randomBody part method return value*/
+catGame.favoriteBodyPart = getRandomBodyPart(catGame.catBody); 
+
+const pet = () => {
 
     /*get cadBody div and find all its children and set it to catBodyContainer*/
     const catBodyContainer = $(".catBody").children();
@@ -18,38 +26,29 @@ const userEvents = () => {
     its children elements i.e. head, back, tail*/
     for(let i = 0; i < catBodyContainer.length; i++){
         $(catBodyContainer[i]).click(function(){
-            console.log($(catBodyContainer[i]).data().part);
+            let petting = $(catBodyContainer[i]).data().part;
+            console.log(petting);
+
+            if(petting === catGame.favoriteBodyPart){
+                catGame.correctPetCount++; /*tracking correct pets*/
+                console.log(`CORRECT PET COUNT ${catGame.correctPetCount}`);
+                catGame.happiness++; /*adding to cat happiness level*/
+                console.log(`HAPPINESS: ${catGame.happiness}`);
+                if(catGame.happiness === 10){ /*win condition*/
+                    console.log('You win!'); /*game win screen*/
+                }
+        
+                if(catGame.correctPetCount == 1){
+                    catGame.correctPetCount = 0; /*must set to 0 again or else it never randomizes again*/
+                    catGame.favoriteBodyPart = getRandomBodyPart(catGame.catBody);  /*randomize catBody part again*/
+                }
+            }
+            
         });
     }
-
 }
+pet();
 
-userEvents();
-
-const getRandomBodyPart = (catBody) => {
-    /*goes from 0 to 5 to correspond with array elements of catBody*/
-    let random = Math.floor((Math.random() * 6)); 
-    return catBody[random]; /*returning the randomized body part*/
-}
 
 /*this will call the bodyRandomize function with the parameter of catBody inside catGame.stats*/
-getRandomBodyPart(catGame.catBody); 
 
-
-
-const pet = (catBodyPart) => {
-    let petting;
-
-    if(petting === catBody){
-        correctPettingCount++; /*tracking correct pets*/
-        catGame.stats.happiness++; /*adding to cat happiness level*/
-        if(catGame.stats.happiness === 10){ /*win condition*/
-            console.log('You win!'); /*game win screen*/
-        }
-
-        if(correctPettingCount == 2){
-            getRandomBodyPart(); /*randomize catBody part again*/
-        }
-    }
-
-}
