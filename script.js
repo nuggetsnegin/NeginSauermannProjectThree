b82516c270b4c06f8d551969eee01756f943addd
Negin <nuggetnegin@gmail.com>
2019-11-15 12:36:25 -0500

    remove cat angry image on click

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ $(function () {
                     console.log(`wrong body part`);
                     checkHappiness();
                     $('.catBody').addClass('no');
-                    $('.catBody').css('background-image', 'url(./assets/cat-2.png)');
+                    // $('.catBody').css('background-image', 'url(./assets/cat-2.png)');
                 } else console.log('error');
             })
         };
